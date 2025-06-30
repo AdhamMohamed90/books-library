97b935ef8497bf501010296dc03dfd6441c35e08
AdhamMohamed90 <adhammohamedhassan2582007@gmail.com>
2025-07-01 00:20:07 +0300

    Add files via upload

---
+++ mains.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adhamcocacolavm;
+
+/**
+ *
+ * @author DELL
+ */
+public class mains {
+    public static void main(String[] args) {
+        new Main().setVisible(true);
+    }
+}
